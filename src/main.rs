49e9d4554a1d31d2836f803ea86a772803f5de94
Rasmus <rasmus.kallqvist@gmail.com>
2021-04-27 13:05:44 +0200

    Switch to using fork of pancurses

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-mod keyboard; // put this in a lib to get rid off unused warnings?
+mod keyboard;
 extern crate pdcurses;
-use pdcurses::{COLS, LINES};
+extern crate pancurses;
+// use pdcurses::{COLS, LINES};
+// use std::ffi::CString;
+
+// const SCREEN_WIDTH: i32 = 84;
+// const SCREEN_HEIGHT: i32 = 20;
+
+fn term_lines() -> i32 {
+    unsafe {
+        pancurses::LINES
+    }
+}
+
+fn term_columns() -> i32 {
+    unsafe {
+        pancurses::COLS
+    }
+}
 
 fn main() {
     unsafe {
-        let window = pdcurses::initscr();
+        let window = pancurses::initscr();
         let mut keyboard_handler = keyboard::KeyboardHandler::new();
         let mut i = 0;
 
@@ -15,14 +32,25 @@ fn main() {
             }
             i += 1;
 
-            pdcurses::werase(window);
-            pdcurses::resize_term(0, 0);
-            pdcurses::curs_set(0);
-            pdcurses::wprintw(window, format!("i = {}\n\0", i).as_ptr() as *const i8);
-            pdcurses::wprintw(window, format!("COLS = {}, LINES = {}\n\0", COLS, LINES).as_ptr() as *const i8);
-            pdcurses::wrefresh(window);
+            window.erase();
+            pancurses::resize_term(0, 0);
+            pancurses::curs_set(0);
+            window.printw(format!("i = {}\n", i));
+            window.printw(format!("COLS = {}, LINES = {}\n", term_columns(), term_lines()));
+            window.refresh();
         }
-        pdcurses::endwin();
+        pancurses::endwin();
     }
 }
 
+// fn top_screen_margin() -> u32 {
+//     unsafe {
+//         ((pdcurses::LINES - SCREEN_HEIGHT) / 2) as u32
+//     }
+// }
+
+// fn left_screen_margin() -> u32 {
+//     unsafe {
+//         ((pdcurses::COLS - SCREEN_WIDTH) / 2) as u32
+//     }
+// }
